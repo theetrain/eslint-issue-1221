796abeb7cd0b6151185a84cf02ae5cd36016a6b0
Enrico Sacchetti <enrico@theetrain.ca>
2021-03-29 10:26:23 -0400

    init repo

---
+++ example.js
@@ -0,0 +1,5 @@
+const test = function ({ too, many, parameters, in, here, wouldnt, you, agree, everyone, ...rest }) {
+  return too;
+};
+
+module.exports = test;
---
+++ fixture.js
@@ -0,0 +1,16 @@
+const test = function ({
+  too,
+  many,
+  parameters,
+  in,
+  here,
+  wouldnt,
+  you,
+  agree,
+  everyone,
+  ...rest
+}) {
+  return too
+}
+
+module.exports = test
